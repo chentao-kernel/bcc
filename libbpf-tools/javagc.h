4e34eb71043171a5d6b2dfca2bfde46180adaa4d
chentao.ct <chentao.kernel@linux.alibaba.com>
2022-06-29 17:52:44 +0800

    libbpf-tools: Add javagc monitor
    Now usdt tracing has been supported in libbpf, so we add
    the javagc monitor as an example with this feature. Normally,
    you can use the command "readelf -n binary" to find the
    usdt in the binary. The javagc tracing result like this:
    Tracing javagc time... Hit Ctrl-C to end.
    time:20:15:08, cpu:3, pid:90640, gc time(us):33764
    time:20:16:29, cpu:1, pid:90640, gc time(us):42339
    time:20:18:10, cpu:0, pid:90640, gc time(us):30843
    
    Signed-off-by: chentao.ct <chentao.kernel@linux.alibaba.com>

---
+++ libbpf-tools/javagc.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+/*
+ * Author: Chen Tao
+ * Create: Wed Jun 29 16:00:19 2022
+ */
+#ifndef __JAVAGC_H
+#define __JAVAGC_H
+
+struct data_t {
+    __u32 cpu;
+    __u32 pid;
+    __u64 ts;
+};
+
+#endif /* __JAVAGC_H */
