7f7ecbc8345cefe426b0edd5a2093df5eeceff43
Tao Chen <chen.dylane@gmail.com>
2024-01-26 18:25:58 +0800

    libbpf-tools: Add offcpu tool
    test case:
    ---------------------------------------------
    
    int main(void)
    {
            while (1) {
                    sleep(1);
            }
            return 0;
    }
    --------------------------------------------
    
    test info:
    --------------------------------------------
    waker:swapper/0, rq=>cpu:0, off=>on:0
    target:sleep, rq=>cpu:9, off=>on:1000092
    waker stack info:
        bpf_prog_6e220a312c30e93e_shched_wakeup_hook
        bpf_prog_6e220a312c30e93e_shched_wakeup_hook
        bpf_trace_run1
        __bpf_trace_sched_wakeup_template
        __traceiter_sched_wakeup
        ttwu_do_wakeup
        ttwu_do_activate
        try_to_wake_up
        wake_up_process
        hrtimer_wakeup
        __hrtimer_run_queues
        hrtimer_interrupt
        __sysvec_apic_timer_interrupt
        sysvec_apic_timer_interrupt
        asm_sysvec_apic_timer_interrupt
        native_safe_halt
        arch_cpu_idle
        default_idle_call
        cpuidle_idle_call
        do_idle
        cpu_startup_entry
        rest_init
        arch_call_rest_init
        start_kernel
        x86_64_start_reservations
        x86_64_start_kernel
        secondary_startup_64_no_verify
        [Missed User Stack]
        -                ���0� (0)
    woker stack info:
        bpf_prog_de8ddf9af94e29b6_sched_switch_hook
        bpf_prog_de8ddf9af94e29b6_sched_switch_hook
        bpf_trace_run3
        __bpf_trace_sched_switch
        __traceiter_sched_switch
        __schedule
        schedule
        do_nanosleep
        hrtimer_nanosleep
        common_nsleep
        __x64_sys_clock_nanosleep
        do_syscall_64
        entry_SYSCALL_64_after_hwframe
        clock_nanosleep
        -                SDx0� (3906361)
    --------------------------------------------
    
    Signed-off-by: Tao Chen <chen.dylane@gmail.com>

---
+++ libbpf-tools/run_loop.c
@@ -0,0 +1,14 @@
+/*
+ * Create: Mon Jan 29 11:47:38 2024
+ */
+#include <stdio.h>
+
+int main()
+{
+	int count = 0;
+	while (1) {
+		count++;
+	}
+	return 0;
+}
+
---
+++ libbpf-tools/sleep.c
@@ -0,0 +1,16 @@
+/*
+ * Create: Sun Jan 28 01:06:54 2024
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void)
+{
+	while (1) {
+		sleep(1);
+	}
+	return 0;
+}
+
